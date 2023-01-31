810b89445bb1c714d49731eb7e2fb20ce07c492b
yolocat-dev <81561018+yolocat-dev@users.noreply.github.com>
2023-01-31 15:07:37 +0100

    Add support for Organization Accounts

--- AltStore/Operations/AuthenticationOperation.swift
+++ AltStore/Operations/AuthenticationOperation.swift
@@ -448,7 +448,11 @@ private extension AuthenticationOperation
     {
         func selectTeam(from teams: [ALTTeam])
         {
-            if let team = teams.first(where: { $0.type == .individual })
+            if let team = teams.first(where: { $0.type == .organization })
+            {
+                return completionHandler(.success(team))
+            }
+            else if let team = teams.first(where: { $0.type == .individual })
             {
                 return completionHandler(.success(team))
             }
